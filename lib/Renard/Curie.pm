4415d834d87a6d0ef3ea16a6d382fa32604f9d62
Zakariyya Mughal <zaki.mughal@gmail.com>
2017-07-11 23:42:52 -0500

    Prep for release v0.003

--- lib/Renard/Curie.pm
+++ lib/Renard/Curie.pm
@@ -3,3 +3,9 @@ package Renard::Curie;
 # ABSTRACT: A document reader written with GTK+.
 
 1;
+
+__END__
+
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/curie/>
