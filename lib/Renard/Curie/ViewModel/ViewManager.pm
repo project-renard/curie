430a5e02aa1eb81ac8103da85ea8544e22c9f663
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-05-04 21:48:35 -0500

    Add current_sentence_number attribute to ViewManager
    This tracks the current sentence being used for TTS.

--- lib/Renard/Curie/ViewModel/ViewManager.pm
+++ lib/Renard/Curie/ViewModel/ViewManager.pm
@@ -3,7 +3,7 @@ package Renard::Curie::ViewModel::ViewManager;
 # ABSTRACT: Manages the currently open views
 
 use Moo;
-use Renard::Incunabula::Common::Types qw(InstanceOf Path FileUri PositiveInt);
+use Renard::Incunabula::Common::Types qw(InstanceOf Path FileUri PositiveInt PositiveOrZeroInt);
 use Renard::Incunabula::Document::Types qw(DocumentModel ZoomLevel);
 use Renard::Incunabula::Format::PDF::Document;
 
@@ -58,8 +58,24 @@ has current_view => (
 	trigger => 1, # _trigger_current_view
 );
 
+=attr current_sentence_number
+
+TODO
+
+=cut
+has current_sentence_number => (
+	is => 'rw',
+	isa => PositiveOrZeroInt,
+	trigger => 1, # _trigger_current_sentence_number
+	default => 0,
+);
+
 method _trigger_current_view($view) {
 	$self->signal_emit( 'update-view' => $view );
+}
+
+method _trigger_current_sentence_number($new_current_sentence_number) {
+	$self->signal_emit( 'update-view' => $self->current_view );
 }
 
 method _trigger_number_of_columns($new_number_of_columns) {
