97b1a73d25e44fe722e3df660ccee5acb65c0fc2
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-05-12 11:53:51 -0400

    Move the current sentence highlighting to a role

--- lib/Renard/Curie/Component/PageDrawingArea.pm
+++ lib/Renard/Curie/Component/PageDrawingArea.pm
@@ -170,7 +170,6 @@ method on_draw_page_cb( (InstanceOf['Cairo::Context']) $cr ) {
 	$self->set_navigation_buttons_sensitivity;
 
 	$self->view->draw_page( $self->drawing_area, $cr );
-	$self->on_draw_page_cb_highlight( $cr );
 
 	my $page_number = $self->view->page_number;
 	if( $self->view->can('_first_page_in_viewport') ) {
@@ -179,39 +178,6 @@ method on_draw_page_cb( (InstanceOf['Cairo::Context']) $cr ) {
 
 	$self->builder->get_object('page-number-entry')
 		->set_text($page_number);
-}
-
-=method on_draw_page_cb_highlight
-
-  method on_draw_page_cb_highlight( (InstanceOf['Cairo::Context']) $cr )
-
-Highlights the current sentence on the page.
-
-=cut
-method on_draw_page_cb_highlight( (InstanceOf['Cairo::Context']) $cr ) {
-	my @top_left = (0,0);
-	if( @{ $self->view_manager->current_text_page } ) {
-		my $sentence = $self->view_manager->current_text_page->[
-			$self->view_manager->current_sentence_number
-		];
-		for my $bbox_str ( @{ $sentence->{bbox} } ) {
-			my $bbox = [ split ' ', $bbox_str ];
-			my $z_bbox = [ map {
-				$_ * $self->view_manager
-					->view_options
-					->zoom_options
-					->zoom_level
-			} @$bbox ];
-			$cr->rectangle(
-				$top_left[0] + $z_bbox->[0],
-				$top_left[1] + $z_bbox->[1],
-				$z_bbox->[2] - $z_bbox->[0],
-				$z_bbox->[3] - $z_bbox->[1],
-			);
-			$cr->set_source_rgba(1, 0, 0, 0.2);
-			$cr->fill;
-		}
-	}
 }
 
 
@@ -251,6 +217,8 @@ with qw(
 	Renard::Curie::Component::PageDrawingArea::Role::PageEntry
 	Renard::Curie::Component::PageDrawingArea::Role::PageLabel
 	Renard::Curie::Component::PageDrawingArea::Role::ScrollWindow
+
+	Renard::Curie::Component::PageDrawingArea::Role::HighlightCurrentSentence
 );
 
 
