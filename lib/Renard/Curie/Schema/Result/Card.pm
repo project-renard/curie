ba793eb01364c3fd3b83cef80817fc5918e63cc0
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-05-29 16:49:32 -0400

    Add columns for Anki flashcard schema

--- lib/Renard/Curie/Schema/Result/Card.pm
+++ lib/Renard/Curie/Schema/Result/Card.pm
@@ -4,55 +4,95 @@ package Renard::Curie::Schema::Result::Card;
 use DBIx::Class::Candy
 	-autotable => v1;
 
+#    id              integer primary key,
+#      -- the epoch milliseconds of when the card was created
 primary_column id => {
 	data_type => 'integer',
 	is_auto_increment => 1,
 };
 
-#    id              integer primary key,
-#      -- the epoch milliseconds of when the card was created
 #    nid             integer not null,--
 #      -- notes.id
-#    did             integer not null,
-#      -- deck id (available in col table)
+column nid => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    ord             integer not null,
 #      -- ordinal : identifies which of the card templates it corresponds to
 #      --   valid values are from 0 to num templates - 1
+column ord => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    mod             integer not null,
 #      -- modificaton time as epoch seconds
-#    usn             integer not null,
-#      -- update sequence number : used to figure out diffs when syncing.
-#      --   value of -1 indicates changes that need to be pushed to server.
-#      --   usn < server usn indicates changes that need to be pulled from server.
+column mod => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    type            integer not null,
 #      -- 0=new, 1=learning, 2=due, 3=filtered
+column type => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    queue           integer not null,
 #      -- -3=sched buried, -2=user buried, -1=suspended,
 #      -- 0=new, 1=learning, 2=due (as for type)
 #      -- 3=in learning, next rev in at least a day after the previous review
+column queue => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    due             integer not null,
 #     -- Due is used differently for different card types:
 #     --   new: note id or random int
 #     --   due: integer day, relative to the collection's creation time
 #     --   learning: integer timestamp
+column due => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    ivl             integer not null,
 #      -- interval (used in SRS algorithm). Negative = seconds, positive = days
+column ivl => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    factor          integer not null,
 #      -- factor (used in SRS algorithm)
+column factor => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    reps            integer not null,
 #      -- number of reviews
+column reps => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    lapses          integer not null,
 #      -- the number of times the card went from a "was answered correctly"
 #      --   to "was answered incorrectly" state
+column lapses => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    left            integer not null,
 #      -- reps left till graduation
-#    odue            integer not null,
-#      -- original due: only used when the card is currently in filtered deck
-#    odid            integer not null,
-#      -- original did: only used when the card is currently in filtered deck
-#    flags           integer not null,
-#      -- currently unused
-#    data            text not null
-#      -- currently unused
+column left => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
 
 1;
--- lib/Renard/Curie/Schema/Result/Revlog.pm
+++ lib/Renard/Curie/Schema/Result/Revlog.pm
@@ -6,24 +6,58 @@ use DBIx::Class::Candy
 
 #    id              integer primary key,
 #       -- epoch-milliseconds timestamp of when you did the review
+primary_column id => {
+	data_type => 'integer',
+};
+
 #    cid             integer not null,
 #       -- cards.id
-#    usn             integer not null,
-#        -- update sequence number: for finding diffs when syncing.
-#        --   See the description in the cards table for more info
+column cid => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    ease            integer not null,
 #       -- which button you pushed to score your recall.
 #       -- review:  1(wrong), 2(hard), 3(ok), 4(easy)
 #       -- learn/relearn:   1(wrong), 2(ok), 3(easy)
+column ease => {
+
+};
+
 #    ivl             integer not null,
 #       -- interval
+column ivl => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    lastIvl         integer not null,
 #       -- last interval
+column lastIvl => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    factor          integer not null,
 #      -- factor
+column factor => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    time            integer not null,
 #       -- how many milliseconds your review took, up to 60000 (60s)
+column time => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
+
 #    type            integer not null
 #       --  0=learn, 1=review, 2=relearn, 3=cram
+column type => {
+	data_type => 'integer',
+	is_nullable => 0,
+};
 
 1;
