24ceee80d5a3c985b3ce9cd4f0d456258f15cd1f
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-07-20 03:51:51 -0500

    Draw grid of PDF pages rendered as PNGs in SVG

--- lib/Renard/Curie/Model/View/Grid/PageActor.pm
+++ lib/Renard/Curie/Model/View/Grid/PageActor.pm
@@ -1,14 +1,20 @@
+use Renard::Incunabula::Common::Setup;
 package Renard::Curie::Model::View::Grid::PageActor;
 # ABSTRACT: A jacquard actor for a document page
 
 use Mu;
+use Renard::Block::Format::Cairo::Types qw(RenderableDocumentModel);
+use Renard::Taffeta::Graphics::Image::PNG;
+use Renard::Yarn::Graphene;
 
 extends qw(Renard::Jacquard::Actor);
 
-has [qw(document page_number)] => (
-	is => 'ro',
-	required => 1,
+ro document => (
+	isa => RenderableDocumentModel,
 );
+
+ro 'page_number';
+
 
 lazy _rendered_page => method() {
 	$self->document->get_rendered_page(
@@ -16,11 +22,30 @@ lazy _rendered_page => method() {
 	);
 };
 
-lazy height => method() { $self->_rendered_page->get_height };
-lazy width => method() { $self->_rendered_page->get_width };
+lazy height => method() { $self->_rendered_page->height };
+lazy width => method() { $self->_rendered_page->width };
+
+lazy size => method() {
+	Renard::Yarn::Graphene::Size->new(
+		height => $self->height,
+		width => $self->width,
+	);
+};
+
+lazy bounds => method() {
+	Renard::Yarn::Graphene::Rect->new(
+		origin => $self->origin_point,
+		size => $self->size,
+	);
+};
 
 method render($svg) {
-	my $rp = $document->_rendered_page;
+	my $rp = $self->_rendered_page;
+	my $taffeta = Renard::Taffeta::Graphics::Image::PNG->new(
+		data => $rp->png_data,
+		origin => $self->origin_point,
+	);
+	$taffeta->render_svg( $svg );
 }
 
 with qw(Renard::Jacquard::Role::Geometry::Position2D);
--- t-grid.pl
+++ t-grid.pl
@@ -8,6 +8,7 @@ use lib "$FindBin::Bin/../lib";
 use Modern::Perl;
 use Renard::Block::Format::PDF::Document;
 use Renard::Curie::Model::View::Grid::PageActor;
+use Renard::Jacquard::Layout::Grid;
 use Path::Tiny;
 
 sub main {
@@ -15,13 +16,41 @@ sub main {
 		filename => path('~/Downloads/Anatomy Shelf Notes copy.pdf'),
 	);
 
-	my $actor = Renard::Curie::Model::View::Grid::PageActor->new(
-		document => $document,
-		page_number => 1,
+	my $_LayoutGroup = Moo::Role->create_class_with_roles(
+		'Renard::Jacquard::Actor' => qw(
+		Renard::Jacquard::Role::Geometry::Position2D
+		Renard::Jacquard::Role::Render::QnD::SVG::Group
+		Renard::Jacquard::Role::Render::QnD::Layout::Grid
+		Renard::Jacquard::Role::Render::QnD::Bounds::Group
+	));
+	my $group = $_LayoutGroup->new(
+		layout => Renard::Jacquard::Layout::Grid->new( rows => 3, columns => 2 ),
 	);
-	use DDP; p $actor;
 
-	require Carp::REPL; Carp::REPL->import('repl'); repl();#DEBUG
+	for my $page_no (1..6) {
+		my $actor = Renard::Curie::Model::View::Grid::PageActor->new(
+			document => $document,
+			page_number => $page_no,
+		);
+		$group->add_child( $actor );
+	}
+
+	$group->x->value( 0 );
+	$group->y->value( 0 );
+	$group->update_layout;
+	my $bounds = $group->bounds;
+
+	my $svg = SVG->new;
+	$group->render($svg);
+
+	$svg->{-childs}[0]->setAttribute('height', $bounds->size->height);
+	$svg->{-childs}[0]->setAttribute('width', $bounds->size->width);
+	my $svg_file = path('a.svg');
+	$svg_file->spew_utf8( $svg->xmlify );
+	use Browser::Open qw(open_browser);
+	open_browser($svg_file);
+
+	#require Carp::REPL; Carp::REPL->import('repl'); repl();#DEBUG
 }
 
 main;
