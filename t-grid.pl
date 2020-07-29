6cdebb4bb00f684b2c554f49f01d3cec4291dc44
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-07-29 16:07:29 -0500

    Implement Grid[Grid[Box[Page]]] scene-graph

--- t-grid.pl
+++ t-grid.pl
@@ -6,38 +6,74 @@ use FindBin;
 use lib "$FindBin::Bin/../lib";
 
 use Modern::Perl;
+use Renard::Incunabula::Common::Setup;
+use feature qw(current_sub);
 use Renard::Block::Format::PDF::Document;
 use Renard::Curie::Model::View::Grid::PageActor;
 use Renard::Jacquard::Layout::Grid;
+use Renard::Jacquard::Layout::Box;
 use Path::Tiny;
 
-sub main {
-	my $document = Renard::Block::Format::PDF::Document->new(
-		filename => path('~/Downloads/Anatomy Shelf Notes copy.pdf'),
-	);
+use constant BOX_LAYOUT => 1;
 
-	my $_LayoutGroup = Moo::Role->create_class_with_roles(
-		'Renard::Jacquard::Actor' => qw(
-		Renard::Jacquard::Role::Geometry::Position2D
-		Renard::Jacquard::Role::Render::QnD::SVG::Group
-		Renard::Jacquard::Role::Render::QnD::Layout::Grid
-		Renard::Jacquard::Role::Render::QnD::Bounds::Group
-	));
+my $document = Renard::Block::Format::PDF::Document->new(
+	filename => path('~/Downloads/Anatomy Shelf Notes copy.pdf'),
+);
+
+my $_LayoutGroup = Moo::Role->create_class_with_roles(
+	'Renard::Jacquard::Actor' => qw(
+	Renard::Jacquard::Role::Geometry::Position2D
+	Renard::Jacquard::Role::Geometry::Size2D
+	Renard::Jacquard::Role::Render::QnD::SVG::Group
+	Renard::Jacquard::Role::Render::QnD::Layout
+	Renard::Jacquard::Role::Render::QnD::Size::Direct
+	Renard::Jacquard::Role::Render::QnD::Bounds::Direct
+));
+
+fun create_group( :$start, :$end, :$margin = 10 ) {
 	my $group = $_LayoutGroup->new(
 		layout => Renard::Jacquard::Layout::Grid->new( rows => 3, columns => 2 ),
 	);
 
-	for my $page_no (1..6) {
+	for my $page_no ($start..$end) {
 		my $actor = Renard::Curie::Model::View::Grid::PageActor->new(
 			document => $document,
 			page_number => $page_no,
 		);
-		$group->add_child( $actor );
+		if( BOX_LAYOUT ) {
+			my $box = $_LayoutGroup->new(
+				layout => Renard::Jacquard::Layout::Box->new( margin => $margin ),
+			);
+			$box->add_child( $actor );
+			$group->add_child( $box );
+		} else {
+			$group->add_child( $actor );
+		}
 	}
+
+	$group;
+}
+
+sub main {
+	my $group = $_LayoutGroup->new(
+		layout => Renard::Jacquard::Layout::Grid->new( rows => 2, columns => 2 ),
+	);
+
+	$group->add_child( create_group(start => 1, end => 6,   margin => 10) );
+	$group->add_child( create_group(start => 7, end => 12,  margin => 50) );
+	$group->add_child( create_group(start => 13, end => 18, margin => 100) );
+	$group->add_child( create_group(start => 19, end => 24, margin => 150) );
 
 	$group->x->value( 0 );
 	$group->y->value( 0 );
-	$group->update_layout;
+
+	sub update_layouts {
+		my ($g) = @_;
+		__SUB__->($_) for @{ $g->children };
+		$g->update_layout if $g->can('update_layout');
+	};
+	update_layouts($group);
+
 	my $bounds = $group->bounds;
 
 	my $svg = SVG->new;
